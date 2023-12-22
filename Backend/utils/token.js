2082136e9d1f0ea442d20bb5c0f2ca1616fea3cd
RezaPratama <63398405+HillarioRZA@users.noreply.github.com>
2023-12-22 13:41:57 +0700

    Delete Backend directory

--- Backend/utils/token.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const jwtSecret = process.env.JWT_SECRET;
-
-const createNewToken = (payload) => {
-    return jwt.sign({ userId: payload }, jwtSecret, { expiresIn: '10d' });
-}
-
-module.exports = { createNewToken }
