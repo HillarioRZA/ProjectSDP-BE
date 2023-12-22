35ed7f06b4ea7c60b863c38609d847d676b0b724
RezaPratama <63398405+HillarioRZA@users.noreply.github.com>
2023-12-22 13:49:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ mongoose
 app.use("/", Routes);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: "Not Found" });
+  res.status(404).json({ message: "Hello Error" });
 });
 
 app.listen(PORT, () => {
